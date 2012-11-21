0fd578f1c54e9aaf4491c8a56da7ae665fa6d4bd
Iyer Arvind <iyer.arvind.sundaram@gmail.com>
2012-11-21 20:53:30 +0530

    Implemented graph

--- src/CAst_main.cpp
+++ src/CAst_main.cpp
@@ -10,18 +10,18 @@ extern FILE* yyin;
 int main(int argc,char **argv)
 {
 
-	//CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
+	CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
       
-	//yyin=fopen(argv[1],"r");
-	//yyparse();
+	yyin=fopen(argv[1],"r");
+	yyparse();
 
 
-	//std::cout<<"\n"<<root->code()<<"\n";
+	std::cout<<"\n"<<root->graph()<<"\n";
 
-	CAst::Ptr::storage_class_specifier scs(_T("register"));
-	CAst::Ptr::declaration_specifiers ts(_T("register"));
-	ts->append(_T("int"));
-	std::cout<<ts->code();
+	//CAst::Ptr::storage_class_specifier scs(_T("register"));
+	//CAst::Ptr::declaration_specifiers ts(_T("register"));
+	//ts->append(_T("int"));
+	//std::cout<<ts->code();
 	return 0;
 }
 
