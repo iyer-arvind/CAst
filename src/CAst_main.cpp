00d32cf74df554fda6374ee058b9d304cd6e1cfc
Iyer Arvind <iyer.arvind.sundaram@gmail.com>
2012-11-22 07:20:08 +0530

    Valued token bug fixed

--- src/CAst_main.cpp
+++ src/CAst_main.cpp
@@ -10,18 +10,23 @@ extern FILE* yyin;
 int main(int argc,char **argv)
 {
 
-	CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
+	//CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
       
-	yyin=fopen(argv[1],"r");
-	yyparse();
+	//yyin=fopen(argv[1],"r");
+	//yyparse();
+	//std::cout<<"\n"<<root->graph()<<"\n";
 
-
-	std::cout<<"\n"<<root->graph()<<"\n";
+	CAst::Ptr::multiplicative_expression_item mi(new CAst::multiplicative_expression_item(_T("*"),_T("10",true)));
+	CAst::Ptr::multiplicative_expression m(_T("a",true));m->append(mi);
+	CAst::Ptr::argument_expression_list ael(_T("\"\%d\"",true));ael->append(new CAst::argument_expression_list_item(_T(","),m));
+	CAst::Ptr::postfix_expression_1 pe(new CAst::postfix_expression_1(_T("printf",true),ael));
+	
 
 	//CAst::Ptr::storage_class_specifier scs(_T("register"));
 	//CAst::Ptr::declaration_specifiers ts(_T("register"));
 	//ts->append(_T("int"));
-	//std::cout<<ts->code();
+	CAst::Ptr::CAst r=pe;
+	std::cout<<r->code();
 	return 0;
 }
 
