09c40bc9608583e41864dcd8c985182e56a7e0b3
Iyer Arvind <iyer.arvind.sundaram@gmail.com>
2012-11-23 19:49:48 +0530

    For loop with declaration

--- src/CAst_main.cpp
+++ src/CAst_main.cpp
@@ -10,20 +10,18 @@ extern FILE* yyin;
 int main(int argc,char **argv)
 {
 
-	//CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
-      
-	//yyin=fopen(argv[1],"r");
-	//yyparse();
-	//std::cout<<"\n"<<root->graph()<<"\n";
+	CAst::ReferenceCountedAutoPointer<CAst::translation_unit> root=parseFile(argv[1]);
+	yyin=fopen(argv[1],"r");
+	yyparse();
+	std::cout<<"\n"<<root->graph()<<"\n";
 
-	CAst::multiplicative_expression_item a={_T("*"),_V("10")};
-	CAst::Ptr::multiplicative_expression_item mi={_T("*"),_V("10")};
-	CAst::Ptr::multiplicative_expression m(_V("a"));m->append(mi);
-	CAst::Ptr::argument_expression_list ael(_V("\"\%d\""));ael->append(new CAst::argument_expression_list_item(_T(","),m));
-	CAst::Ptr::function_call pe(new CAst::function_call(_V("printf"),ael));
+	//CAst::multiplicative_expression_item a({_T("*"),_V("10")});
+	//CAst::Ptr::multiplicative_expression_item mi({_T("*"),_V("10")});
+	//CAst::Ptr::multiplicative_expression m(_V("a"));m->append(mi);
+	//CAst::Ptr::argument_expression_list ael(_V("\"\%d\""));ael->append(new CAst::argument_expression_list_item(_T(","),m));
+	//CAst::Ptr::function_call pe(new CAst::function_call(_V("printf"),ael));
+	//CAst::Ptr::CAst root=pe;
 	
-
-	CAst::Ptr::CAst root=pe;
 	std::cout<<root->graph();
 	return 0;
 }
